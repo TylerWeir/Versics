677654910122feed98167b2b99bd0dd96cc2ac96
TylerWeir <tylerw@live.com>
2020-12-14 00:36:09 -0800

    Major revison to structure, abandoned Nodes and Sticks for direct route.

--- versics.py
+++ versics.py
@@ -6,45 +6,67 @@ from pygame.math import Vector2
 
 class Versics(pygame.sprite.Sprite):
     """Class used to give physics to models with structure."""
-    pass
+
+    def __init__(self, points, old_points, forces):
+        # Lists to contain all the points in the system.
+        self.points = []
+        self.old_points = []
+        self.forces = []
 
 
-class Node():
-    """Class used to represent points in the structure of a physics model."""
+        self.gravity = Vector2((0, -1.0))
+        self.time_step = 0.0
 
-    def __init__(self, position):
-        self.x = position[0]
-        self.y = position[1]
+        # Fill in the lists
+        for i in range(len(points)):
+            self.points.append(Vector2(points[i]))
+            self.old_points.append(Vector2(old_points[i]))
+            self.forces.append(Vector2(forces[i]))
 
-    def setX(self, x):
-        """Sets the x coorinate of the Node."""
-        self.x = x
+    def timeStep(self):
+        accumulate_forces()
+        verlet()
+        satisfy_contraints()
 
-    def setY(self, y):
-        """Sets the y coorindate of the Node."""
-        self.y = y
+    def verlet(self):
+        """Verlet integration step."""
+        for i in range(len(points)):
+            temp = Vector2(self.points[i])
+            old_pos = Vector2(self.old_points[i])
+            a = Vector2(self.forces[i])
 
-    def setPos(self, position):
-        """Sets the position of the Node."""
-        self.x = position[0]
-        self.y = position[1]
+            self.points[i] += temp - old_pos + a*(self.time_step**2)
+            self.old_points[i].update(temp)
 
-    def getX(self):
-        """Returns the x coordinate of the Node."""
-        return self.x
+    def accumulate_forces(self):
+        """Accumulates forces for each particle."""
+        for force in self.forces:
+            force = Vector2(self.gravity)
 
-    def getY(self):
-        """Returns the y coorinate of the Node."""
-        return self.y
+    def satisfy_contraints(self):
+        # Kepps the points inside a box
+        for point in self.points:
+            point.x = min(max(point.x, 0), 1000)
+            point.y = min(max(point.y, 0), 1000)
 
-    def getPos(self):
-        """Returns the position of the Node as a tuple (x, y)."""
-        return (self.x, self.y)
+        # Keeps the points a distance apart
 
 
-class Stick():
-    """Class used to connect Nodes in the structure of a physics model."""
 
-    def __init__(self, breakpoint=1, node1, node2):
-        self.node1 = node1
-        self.node2 = node2
+#Test program
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 1000))
+
+running = True
+while running:
+    # Loops through the event queue.
+    for event in pygame.event.get():
+        # Quit if the user clicks the quit button.
+        if event.type == pygame.QUIT:
+            running = False
+        # Looks for a key pressed event.
+        elif event.type == pygame.KEYDOWN:
+            # Quit if the escape key is pressed.
+            if event.key == pygame.K_ESCAPE:
+                running = False
